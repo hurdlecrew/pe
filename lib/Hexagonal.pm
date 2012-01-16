d0fcaf5981f9f65c7d92b58b62b4082718d8ddfc
Kurt Edmiston <hurdlecrew@gmail.com>
2012-01-15 23:49:09 -0500

    Added POD documentation

--- lib/Hexagonal.pm
+++ lib/Hexagonal.pm
@@ -34,3 +34,28 @@ sub hexagonals
 
 1;
 
+__END__
+
+=head1 NAME
+
+Hexagonal.pm - Utilities for dealing with hexagonal numbers
+
+=head1 SYNOPSIS
+
+    use Hexagonal qw( hexagonal hexagonals );
+    print "$n is hexagonal!" if hexagonal($n);
+    my @first_100_hexagonals = hexagonals(1..100); 
+
+=head1 DESCRIPTION
+
+    A hexagonal number is an integer having the form h(n) = 2n^2-n = n(2n-1). The first few hexabonal numbers are 1, 6, 15, 28, 45.  This module includes the following utilities for dealing with them:
+
+=head2 hexagonal($n)
+
+    Test for hexagonality, that is, hexagonal(45) returns a true value, but hexagonal(44) returns a false value.
+
+=head2 hexagonals(@list)
+
+    For a given list of integers, return the corresponding list of hexagonal numbers.  For example, hexagonals(1,2,3,4,5) will return the list (1,6,15,28,45).
+
+=cut
