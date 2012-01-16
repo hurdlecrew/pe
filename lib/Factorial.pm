a501dcb0e21e2fc71ac71b623f1b97a6435008a2
Kurt Edmiston <hurdlecrew@gmail.com>
2012-01-15 23:31:02 -0500

    Added boilerplate POD

--- lib/Factorial.pm
+++ lib/Factorial.pm
@@ -7,7 +7,21 @@ use base 'Exporter';
 our @EXPORT_OK = qw(
 );
 
-
-
 1;
 
+__END__
+
+=head1 NAME
+
+Factorial.pm - Efficiently compute a factorial
+
+=head1 SYNOPSIS
+
+    use Factorial;
+    ...
+
+=head1 DESCRIPTION
+
+    ...
+
+=cut
